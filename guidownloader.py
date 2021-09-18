f7199d2d671e9e6eddecdbb12de5f7f76fe3c695
TheBest6337 <dj.checker@outlook.de>
2021-09-18 13:59:30 +0200

    Translated to english.

--- guidownloader.py
+++ guidownloader.py
@@ -1,5 +1,5 @@
 from tkinter import *
-import PySimpleGUI as gui
+import PySimpleGUI as os
 from PySimpleGUI.PySimpleGUI import BUTTON_TYPE_BROWSE_FILES, BUTTON_TYPE_BROWSE_FOLDER
 from pytube import YouTube
 import time
@@ -7,18 +7,18 @@ import requests
 import os
 
 layout = [
-    [gui.Text("Yt Downloader", key="video_title")],
-    [gui.Text("Bitte gebe den Link zum Downloaden ein: ")],
-    [gui.Input("", key="input"), gui.Button("Submit"), gui.Text("", key="error")],
-    [gui.Text("Download Type: ")],
-    [gui.Button("Mp4", button_color="gray")],
-    [gui.Button("Mp3", button_color="gray")],
-    [gui.Button("Download"), gui.Button("Download Folder", BUTTON_TYPE_BROWSE_FOLDER)],
-    [gui.Text("", key="downloadfolder")],
-    [gui.Text("", key="DownloadStatus")]
+    [os.Text("YouTube Downloader", key="video_title")],
+    [os.Text("Please enter a YouTube link: ")],
+    [os.Input("", key="input"), os.Button("Submit"), os.Text("", key="error")],
+    [os.Text("Download Type: ")],
+    [os.Button("Mp4", button_color="gray")],
+    [os.Button("Mp3", button_color="gray")],
+    [os.Button("Download"), os.Button("Download Folder", BUTTON_TYPE_BROWSE_FOLDER)],
+    [os.Text("", key="downloadfolder")],
+    [os.Text("", key="DownloadStatus")]
 ]
 
-window = gui.Window(title='Yt Downloader', layout=layout, margins=(480, 270))
+window = os.Window(title='Yt Downloader', layout=layout, margins=(480, 270))
 
 download_type = ""
 download_folder = ""
@@ -32,10 +32,10 @@ while True:
             window["Submit"].Update(button_color = "green")
             window["error"].Update("")
             title = yt.title
-            window["video_title"].Update(f"Videotitel: {title}")
+            window["video_title"].Update(f"Videotitle: {title}")
         except:
-            print("Nutze bitte einen GÜLTIGEN YouTube Link!")
-            window["error"].Update("Nutze bitte einen GÜLTIGEN YouTube Link!")
+            print("Please use a VALID YouTube link!")
+            window["error"].Update("Please use a VALID YouTube link!")
             window["Submit"].Update(button_color = "red")
     elif event == "Mp4":
         window["Mp4"].Update(button_color = "black")
@@ -49,31 +49,31 @@ while True:
     if event == "Download":
         download_folder = values["Download Folder"]
         if download_folder == "":
-            window["DownloadStatus"].Update("Bitte Wähle einen Downloadordner aus!", text_color="red")
-            print("Kein Download Ordner ausgewählt")
+            window["DownloadStatus"].Update("Please select a download folder!", text_color="red")
+            print("No download folder selected")
             break
         else:
             window["downloadfolder"].Update(f"Download Folder: {download_folder}")
             
         if not str(values["input"]):
-            window["DownloadStatus"].Update("Bitte Wähle alles RICHTIG aus!", text_color="red")
-            print("Kein Download Typ ausgewählt")
+            window["DownloadStatus"].Update("Please select everything CORRECTLY!", text_color="red")
+            print("No download type selected")
             break
         if download_type == "":
-            window["DownloadStatus"].Update("Bitte Wähle alles RICHTIG aus!", text_color="red")
-            print("Kein Download Typ ausgewählt")
+            window["DownloadStatus"].Update("Please select everything CORRECTLY!", text_color="red")
+            print("No download type selected")
             break
         if download_type == "mp3":
-            window["DownloadStatus"].Update("Mp3 wird gedownloaded")
+            window["DownloadStatus"].Update("Mp3 will be downloaded")
             ys = yt.streams.get_audio_only() 
             ys.download(download_folder)
             window["DownloadStatus"].Update("Download Complete!")
         elif download_type == "mp4":
-            window["DownloadStatus"].Update("Mp4 wird gedownloaded")
+            window["DownloadStatus"].Update("Mp4 will be downloaded")
             ys = yt.streams.get_highest_resolution()
             ys.download(download_folder)
             window["DownloadStatus"].Update("Download Complete!")
-    if event == gui.WIN_CLOSED:
+    if event == os.WIN_CLOSED:
         break
 
 event, values = window.read()
