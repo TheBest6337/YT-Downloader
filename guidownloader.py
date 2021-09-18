dcfdf21a4ca324d2a198238f0fd3b1f7e0edbe39
TheBest6337 <80114847+TheBest6337@users.noreply.github.com>
2021-09-18 19:23:09 +0200

    Update guidownloader.py

--- guidownloader.py
+++ guidownloader.py
@@ -4,7 +4,6 @@ from PySimpleGUI.PySimpleGUI import BUTTON_TYPE_BROWSE_FILES, BUTTON_TYPE_BROWSE
 from pytube import YouTube
 import time
 import requests
-import os
 
 layout = [
     [os.Text("YouTube Downloader", key="video_title")],
